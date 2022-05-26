eef9a5f705a2b0c3debde669670192aa870c7ad3
Nastasia <lisitsinan@gmail.com>
2022-05-26 20:34:27 +0300

    Кнопки лайка реагируют на нажатие

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 let popup = document.querySelector('.popup');
 let profileEditButton = document.querySelector('.profile__edit-button');
-console.log(profileEditButton);
 let popupCloseButton = document.querySelector('.popup__close-button');
 
 profileEditButton.addEventListener('click', function () {
@@ -10,3 +9,11 @@ profileEditButton.addEventListener('click', function () {
 popupCloseButton.addEventListener('click', function () {
   popup.classList.remove('popup_opened');
 })
+
+let placeLikeButtons = document.querySelectorAll('.place__like-button');
+
+placeLikeButtons.forEach(button => button.addEventListener('click', function ()
+  { console.log(this);
+    this.classList.toggle('place__like-button_active');
+  })
+)
